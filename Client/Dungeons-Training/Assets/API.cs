4ceb6680f59fc383a9d7737ba0f494b11a0b4025
Julian Bieber <julian.bieber.1992@gmail.com>
2020-05-17 11:03:15 +0200

    login

--- Client/Dungeons-Training/Assets/API.cs
+++ Client/Dungeons-Training/Assets/API.cs
@@ -5,18 +5,21 @@ using System.Threading.Tasks;
 using UnityEngine.Networking;
 using UnityEngine;
 using System.Text;
+using System.Linq;
 
 public class API {
-    
-    public static async Task<Option<B>> post<A, B>(string url, A data) {
-        var requestJson = JsonUtility.ToJson(data);
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(requestJson);
-        Debug.Log(requestJson);
-        using (var www = UnityWebRequest.Post(url, requestJson))
+
+    public static async Task<Option<B>> get<A, B>(string url, Dictionary<string, string> queryParemeters) {
+        var urlWithQuery = url + queryString(queryParemeters);
+        using (var www = UnityWebRequest.Get(urlWithQuery))
             {
-                www.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
                 www.SetRequestHeader("Content-Type", "application/json");
                 www.SetRequestHeader("Accept", "application/json");
+
+                if (Global.token.isSome && Global.userId.isSome) {
+                    www.SetRequestHeader("X-userId", Global.userId.value.ToString());
+                    www.SetRequestHeader("X-token", Global.token.value);
+                }
                 www.SendWebRequest();
                 
                 while (!www.isDone)
@@ -28,6 +31,76 @@ public class API {
                     return Option<B>.Some(JsonUtility.FromJson<B>(responseString));
                 }
             }
+    }
+
+    public static async Task<Option<B>> post<A, B>(string url, A data, Dictionary<string, string> queryParemeters) {
+        var requestJson = JsonUtility.ToJson(data);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(requestJson);
+        var urlWithQuery = url + queryString(queryParemeters);
+        using (var www = UnityWebRequest.Post(urlWithQuery, requestJson))
+            {
+                www.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+                www.SetRequestHeader("Content-Type", "application/json");
+                www.SetRequestHeader("Accept", "application/json");
+                if (Global.token.isSome && Global.userId.isSome) {
+                    www.SetRequestHeader("X-userId", Global.userId.value.ToString());
+                    www.SetRequestHeader("X-token", Global.token.value);
+                }
+                www.SendWebRequest();
+                
+                while (!www.isDone)
+                    await Task.Delay(1);
+                var responseString = www.downloadHandler.text;
+                if (www.isNetworkError || www.isHttpError) {
+                    return Option<B>.None;
+                } else {
+                    return Option<B>.Some(JsonUtility.FromJson<B>(responseString));
+                }
+            }
+    }
+
+    public static async Task<Option<B>> put<A, B>(string url, A data, Dictionary<string, string> queryParemeters) {
+        var requestJson = JsonUtility.ToJson(data);
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(requestJson);
+        var urlWithQuery = url + queryString(queryParemeters);
+        using (var www = UnityWebRequest.Put(urlWithQuery, requestJson))
+            {
+                www.uploadHandler = (UploadHandler) new UploadHandlerRaw(bodyRaw);
+                www.SetRequestHeader("Content-Type", "application/json");
+                www.SetRequestHeader("Accept", "application/json");
+                if (Global.token.isSome && Global.userId.isSome) {
+                    www.SetRequestHeader("X-userId", Global.userId.value.ToString());
+                    www.SetRequestHeader("X-token", Global.token.value);
+                }
+                www.SendWebRequest();
+                
+                while (!www.isDone)
+                    await Task.Delay(1);
+                var responseString = www.downloadHandler.text;
+                if (www.isNetworkError || www.isHttpError) {
+                    return Option<B>.None;
+                } else {
+                    return Option<B>.Some(JsonUtility.FromJson<B>(responseString));
+                }
+            }
+    }
+
+    static string queryString(Dictionary<string, string> queryParemeters) {
+        StringBuilder queryBuilder = new StringBuilder();
+        if (queryParemeters.Count > 0) {
+            queryBuilder.Append("?");
+        }
+        
+        foreach (var parameter in queryParemeters)
+        {
+            var key = parameter.Key;
+            var value = parameter.Key;
+            queryBuilder.Append(key);
+            queryBuilder.Append("=");
+            queryBuilder.Append(UnityWebRequest.EscapeURL(value));
+            queryBuilder.Append("&");
+        }
+        return queryBuilder.ToString();
     }
         
 }
---
+++ Client/Dungeons-Training/Assets/Login.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+
+    public InputField userNameField;
+    public InputField passwordField;
+
+    public Text feedbackField;
+    public Button self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async void login() {
+        var loginResponseOption = await AccountAPI.login(userNameField.text, passwordField.text);
+        if (loginResponseOption.isSome) {
+            var loginResponse = loginResponseOption.value;
+            Global.userId = Option<int>.Some(loginResponse.Item1);
+            Global.token = Option<string>.Some(loginResponse.Item2);
+            feedbackField.text = "Login Success"; // TODO switch scene
+        } else {
+            feedbackField.text = "Failed to login";
+        }
+    }
+}
