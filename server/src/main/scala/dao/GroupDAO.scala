1e8c4e16c2d33ca16c6a58d7896974d53ba8fad9
Julian Bieber <j.bieber@patagona.de>
2020-05-19 11:46:32 +0200

    allow users to only be part of one group

--- server/src/main/scala/dao/GroupDAO.scala
+++ server/src/main/scala/dao/GroupDAO.scala
@@ -9,13 +9,17 @@ class GroupDAO @Inject() (userDAO: UserDAO) {
   def addToGroup(user: Int, otherUserName: String): Boolean = {
     userDAO.getUser(otherUserName).map{ case DAOUser(otherUserId , _, _)=>
       GroupDAO.groups.synchronized{
-        GroupDAO.groups.find(_.contains(otherUserId)) match {
-          case Some(existingGroup) => existingGroup += user
-          case None =>
-            val group = new mutable.ListBuffer[Int]
-            group += user
-            group += otherUserId
-            GroupDAO.groups += group
+        if (!GroupDAO.groups.exists(_.contains(user))) {
+          GroupDAO.groups.find(_.contains(otherUserId)) match {
+            case Some(existingGroup) =>
+              if (!existingGroup.contains(user))
+                existingGroup += user
+            case None =>
+              val group = new mutable.ListBuffer[Int]
+              group += user
+              group += otherUserId
+              GroupDAO.groups += group
+          }
         }
       }
     }.isDefined
