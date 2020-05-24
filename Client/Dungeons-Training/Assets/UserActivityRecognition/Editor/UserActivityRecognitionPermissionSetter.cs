f0b9eb64f6a7e817f69dada5a5f4957ad60648b1
Julian Bieber <julian.bieber.1992@gmail.com>
2020-05-24 14:06:45 +0200

    added UserActivityRecognition lib

---
+++ Client/Dungeons-Training/Assets/UserActivityRecognition/Editor/UserActivityRecognitionPermissionSetter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+using System.IO;
+
+namespace KKUserActivityRecognition {
+		public class SetSpeechRecognitionPermissionsOniOS {
+
+		public static bool shouldRun = true;
+		/* check readme.pdf for explanation on those keys */
+		public static string motionUsageDescription = "Recognizing user activity.";
+
+		private static string nameOfPlist = "Info.plist";
+		private static string keyForMotionUsage = "NSMotionUsageDescription";
+
+		#if UNITY_IOS
+		[PostProcessBuild]
+		public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject) {
+
+			if (shouldRun && buildTarget == BuildTarget.iOS) {
+
+				// Get plist
+				string plistPath = pathToBuiltProject + "/" + nameOfPlist;
+				PlistDocument plist = new PlistDocument();
+				plist.ReadFromString(File.ReadAllText(plistPath));
+
+				// Get root
+				PlistElementDict rootDict = plist.root;
+
+				rootDict.SetString(keyForMotionUsage, motionUsageDescription);
+
+				// Write to file
+				File.WriteAllText(plistPath, plist.WriteToString());
+			}
+		}
+		#endif
+
+	}
+}
+
