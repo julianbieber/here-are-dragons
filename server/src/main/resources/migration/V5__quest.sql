a045a8ce46e872ce1867e415c528ef97fe7c4bba
Julian Bieber <j.bieber@patagona.de>
2020-05-26 12:37:45 +0200

    fix v5

--- server/src/main/resources/migration/V5__quest.sql
+++ server/src/main/resources/migration/V5__quest.sql
@@ -2,7 +2,7 @@ create table public.quest (
     id serial unique not null,
     longitude float8 not null,
     latitude float8 not null
-)
-INSERT INTO public.SampleQuest  ( longitude, latitude) VALUES ( 24.968580,60.191880)
-INSERT INTO public.SampleQuest  (longitude, latitude) VALUES ( 24.968589,60.191887)
-INSERT INTO public.SampleQuest  ( longitude, latitude) VALUES ( 25.968580,60.191880)
+);
+INSERT INTO public.quest  ( longitude, latitude) VALUES ( 24.968580,60.191880);
+INSERT INTO public.quest  (longitude, latitude) VALUES ( 24.968589,60.191887);
+INSERT INTO public.quest  ( longitude, latitude) VALUES ( 25.968580,60.191880);
