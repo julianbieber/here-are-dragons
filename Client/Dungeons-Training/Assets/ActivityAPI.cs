46e5345517227e4e7e5553eddfefcb101c90a790
Julian Bieber <julian.bieber.1992@gmail.com>
2020-05-26 12:43:27 +0200

    send activity start/end to server

---
+++ Client/Dungeons-Training/Assets/ActivityAPI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ActivityAPI
+{
+    public static async void startActivity(string activityType) {
+        await API.put<ActivityRequest, string>(Global.baseUrl + "activity", new ActivityRequest{ activityType = activityType}, new Dictionary<string, string>()); 
+    }
+
+    public static async void stopActivity(string activityType) {
+        await API.delete<string>(Global.baseUrl + "activity", new Dictionary<string, string>());
+    }
+}
+
+public class ActivityRequest {
+    public string activityType;
+}
---
+++ Client/Dungeons-Training/Assets/ActivityTracker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KKUserActivityRecognition;
+using System.Threading.Tasks;
+
+public class ActivityTracker : MonoBehaviour
+{
+    private Option<ActivityType> currentActivity = Option<ActivityType>.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool isAvailable = UserActivityRecognition.IsAvailable();
+
+		if (isAvailable) {
+			// make sure your listener prefab exists on current scene!
+			GameObject.FindObjectOfType<UserActivityListener>()
+				.onUserActivityRecognized
+				.AddListener(OnActivityRecognized);
+            UserActivityRecognition.StartUpdates();
+		} else {
+			Debug.Log("Activity Tracker is unavailable");
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    async void OnActivityRecognized(UserActivityInfo activityInfo) {
+        var newType = activityInfo.recognizedType;
+
+        if (!currentActivity.isSome && isKnownActivity(newType)) {
+            currentActivity = Option<ActivityType>.Some(newType);
+            ActivityAPI.startActivity(toActivityString(newType));
+        }
+
+        if (currentActivity.isSome && currentActivity.value != newType && isKnownActivity(newType)) {
+            ActivityAPI.stopActivity(toActivityString(currentActivity.value));
+            await Task.Delay(10);
+            currentActivity = Option<ActivityType>.Some(newType);
+            ActivityAPI.startActivity(toActivityString(newType));
+        }
+
+        if (currentActivity.isSome && currentActivity.value != newType && !isKnownActivity(newType)) {
+            ActivityAPI.stopActivity(toActivityString(currentActivity.value));
+            currentActivity = Option<ActivityType>.None;
+        }
+
+    }
+    
+    bool isKnownActivity(ActivityType t) {
+        return t == ActivityType.cycling || t == ActivityType.running;
+    }
+
+    string toActivityString(ActivityType t) {
+        if (t == ActivityType.cycling) {
+            return "CYCLING";
+        }
+        if (t == ActivityType.running) {
+            return "RUNNING";
+        }
+        throw new System.Exception("Unknown activity");
+    }
+
+    bool matchesCurrentActivity(ActivityType t) {
+        if (currentActivity.isSome) {
+            return t == currentActivity.value;
+        } else {
+            return false;
+        }
+    }
+}
