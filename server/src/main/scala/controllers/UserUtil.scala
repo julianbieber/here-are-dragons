c7fce788bbe5b57240c0ff36215d24e16a57d95d
Julian Bieber <j.bieber@patagona.de>
2020-05-31 12:05:04 +0200

    server returns a charcter summary

--- server/src/main/scala/controllers/UserUtil.scala
+++ server/src/main/scala/controllers/UserUtil.scala
@@ -1,10 +1,11 @@
 package controllers
 
+import com.github.plokhotnyuk.jsoniter_scala.core._
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.Controller
 import dao.UserDAO
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait UserUtil extends Controller {
   def userDAO: UserDAO
@@ -21,6 +22,14 @@ trait UserUtil extends Controller {
     }
   }
 
+  def withUserAuto[A](request: Request)(f: Int => A)(implicit codec: JsonValueCodec[A]): Response = {
+    withUser(request)({ userId =>
+      val r = f(userId)
+      val responseString = writeToString(r)
+      response.ok(responseString)
+    })
+  }
+
   def withUserAsync(request: Request)(f: Int => Future[Response]): Future[Response] = {
     val userId = request.headerMap.get("X-userId").map(_.toInt)
     val token = request.headerMap.get("X-token")
@@ -31,6 +40,13 @@ trait UserUtil extends Controller {
       case _ =>
         Future.successful(response.unauthorized)
     }
+  }
+
+  def withUserAsyncAuto[A](request: Request)(f: Int => Future[A])(implicit ec: ExecutionContext, codec: JsonValueCodec[A]): Future[Response] = {
+    withUserAsync(request)(f(_).map{ r =>
+      val responseString = writeToString(r)
+      response.ok(responseString)
+    })
   }
 
 }
