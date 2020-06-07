9af9fbf1d9066611632488fc1cd3db210bcd283f
Yasemin Göksu <yasemingoksu@eduroam120.igd.fraunhofer.de>
2020-06-07 12:26:23 +0200

    Quest

---
+++ Client/Dungeons-Training/Assets/ErkennenObQuestInNäheVonPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErkennenObQuestInNäheVonPlayer : MonoBehaviour{ 
+
+
+  private void OnTriggerEnter(Collider other){
+	
+  } 
+}
