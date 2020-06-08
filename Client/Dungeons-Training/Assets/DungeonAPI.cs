c728640afbe1ad1722e5b3ad422b0075f9c592cf
Julian Bieber <julian.bieber.1992@gmail.com>
2020-06-08 11:56:16 +0200

    wait fur turn and get the dungeon from server

---
+++ Client/Dungeons-Training/Assets/DungeonAPI.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Linq;
+
+public static class DungeonAPI
+{
+    public static async Task<List<int>> getAvailableDungeonIds() {
+        var response = await API.get<AvailableDungeonsResponse>("dungeons", new Dictionary<string, string>());
+        if (response.isSome) {
+            return response.value.ids;
+        } else {
+            Debug.Log("Failed to retrieve available dungeons");
+            return new List<int>();
+        }
+    }
+
+    public static async Task<Option<Dungeon>> openDungeon(int questId) {
+        var request = new OpenDungeonRequest{ questId = questId };
+        var response = await API.put<OpenDungeonRequest, DungeonResponse>("dungeon", request, new Dictionary<string, string>());
+
+        if (response.isSome) {
+            return Option<Dungeon>.Some(convertDungeonResponse(response.value));
+        } else {
+            Debug.Log("failed to open dungeon");
+            return Option<Dungeon>.None;
+        }
+    }
+
+    public static async void endTurn(int dungeonId, Turn turn) {
+        await API.post<Turn, string>("dungeon/" + dungeonId, turn, new Dictionary<string, string>());
+    }
+
+    public static async Task<Option<Dungeon>> getDungeon(int dungeonId) {
+        var response = await API.get<DungeonResponse>("dungeon/" + dungeonId, new Dictionary<string, string>());
+        if (response.isSome) {
+            return Option<Dungeon>.Some(convertDungeonResponse(response.value));
+        } else {
+            Debug.Log("Failed to retrieve dungeon for id: " + dungeonId);
+            return Option<Dungeon>.None;
+        }
+    }
+
+    static Dungeon convertDungeonResponse(DungeonResponse response) {
+        return new Dungeon {
+            units = response.units.Select(u => convertUnitResponse(u)).ToList(),
+            myTurn = response.myTurn,
+            ap = response.ap
+        };
+    }
+
+    static Unit convertUnitResponse(UnitResponse response) {
+        if (response.tyype == "empty") {
+            return new EmptyUnit {
+                prefabId = response.prefabId
+            };
+        } else if (response.tyype == "npc") {
+            return new NPCUnit {
+                prefabId = response.prefabId,
+                health = response.health
+            };
+        } else if (response.tyype == "player") {
+            return new PlayerUnit {
+                userId = response.userId,
+                health = response.health
+            };
+        } else {
+            throw new System.ArgumentException("Unrecognized unit type", response.tyype);
+        }
+    }
+}
+
+[Serializable]
+public class AvailableDungeonsResponse {
+    public List<int> ids;
+}
+
+[Serializable]
+public class OpenDungeonRequest {
+    public int questId;
+}
+
+[Serializable]
+public class DungeonResponse {
+    public List<UnitResponse> units;
+    public bool myTurn;
+    public int ap;
+}
+
+public class UnitResponse {
+    public string tyype;
+    public int userId;
+    public int health;
+    public int prefabId;
+}
--- Client/Dungeons-Training/Assets/DungeonController.cs
+++ Client/Dungeons-Training/Assets/DungeonController.cs
@@ -14,11 +14,15 @@ public class DungeonController : MonoBehaviour
 
     public Text apText;
 
-    private Dungeon dungeon = new Dungeon {
-        units = new List<Unit>()
-    };
+    public GameObject joinButton;
+    public GameObject openButton;
+    public InputField questIdInputField;
+    public GameObject questIdInputObject;
 
     private Turn currentTurn;
+
+    private Option<int> dungeonId = Option<int>.None;
+    private bool waitForTurn = true;
 
     private int remainingAP;
 
@@ -28,6 +32,8 @@ public class DungeonController : MonoBehaviour
     private Vector3 topLeft;
     private float screenWidth;
     private float screenHeight;
+
+    private int nextUpdate = 1; 
     // Start is called before the first frame update
     void Start()
     {
@@ -40,22 +46,35 @@ public class DungeonController : MonoBehaviour
         topLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0));
         screenHeight = topLeft.y - lowerLeft.y;
 
-        dungeon.units.Add(new EmptyUnit{ prefabId = 0 });
-        dungeon.units.Add(new PlayerUnit{userId = Global.userId.value, health = 100});
-        dungeon.units.Add(new EmptyUnit{ prefabId = 0 });
-        dungeon.units.Add(new NPCUnit{ prefabId = 0, health = 100});
-        dungeon.units.Add(new NPCUnit{ prefabId = 0, health = 100});
-        dungeon.units.Add(new NPCUnit{ prefabId = 0, health = 100});
-        dungeon.units.Add(new NPCUnit{ prefabId = 0, health = 100});
-
         currentTurn = new Turn{ turnId = 0, skillsUsed = new List<SkillUsage>() };
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (objects.Count != dungeon.units.Count) {
+    public async void openDungeon() {
+        var dungeonO = await DungeonAPI.openDungeon(Int32.Parse(questIdInputField.text));
+        if (dungeonO.isSome) {
+            var dungeon = dungeonO.value;
             setDungeon(dungeon);
+            joinButton.SetActive(false);
+            openButton.SetActive(false);
+            questIdInputObject.SetActive(false);
+        }
+    }
+
+    public async void joinDungeon() {
+        // TODO 
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {
+        if (dungeonId.isSome && waitForTurn) {
+            if(Time.time>=nextUpdate) {
+                nextUpdate=Mathf.FloorToInt(Time.time)+2;
+                var dungeonO = await DungeonAPI.getDungeon(dungeonId.value);
+                if (dungeonO.isSome) {
+                    setDungeon(dungeonO.value);
+                }
+            }
         }
 
         apText.text = remainingAP.ToString();
@@ -63,48 +82,50 @@ public class DungeonController : MonoBehaviour
 
     void setDungeon(Dungeon dungeon) {
         foreach(var o in objects) {
-                o.Destroy();
+            o.Destroy();
+        }
+        objects.Clear();
+        var index = 0;
+        foreach(var unit in dungeon.units) {
+            if (unit is PlayerUnit) {
+                var o = Instantiate(playerPrefab);
+                o.GetComponent<DungeonUnit>().Self = unit;
+                o.GetComponent<DungeonUnit>().gm = this;
+                o.GetComponent<DungeonUnit>().index = index;
+                objects.Add(o);
             }
-            objects.Clear();
-            var index = 0;
-            foreach(var unit in dungeon.units) {
-                if (unit is PlayerUnit) {
-                    var o = Instantiate(playerPrefab);
-                    o.GetComponent<DungeonUnit>().Self = unit;
-                    o.GetComponent<DungeonUnit>().gm = this;
-                    o.GetComponent<DungeonUnit>().index = index;
-                    objects.Add(o);
-                }
-                if (unit is NPCUnit) {
-                    var o = Instantiate(npcPrefabs[((NPCUnit)unit).prefabId]);
-                    o.GetComponent<DungeonUnit>().Self = unit;
-                    o.GetComponent<DungeonUnit>().gm = this;
-                    o.GetComponent<DungeonUnit>().index = index;
-                    objects.Add(o);
-                }
-                if (unit is EmptyUnit) {
-                    var o = Instantiate(emptyPrefabs[((EmptyUnit)unit).prefabId]);
-                    o.GetComponent<DungeonUnit>().Self = unit;
-                    o.GetComponent<DungeonUnit>().gm = this;
-                    o.GetComponent<DungeonUnit>().index = index;
-                    objects.Add(o);
-                }
-                ++index;
+            if (unit is NPCUnit) {
+                var o = Instantiate(npcPrefabs[((NPCUnit)unit).prefabId]);
+                o.GetComponent<DungeonUnit>().Self = unit;
+                o.GetComponent<DungeonUnit>().gm = this;
+                o.GetComponent<DungeonUnit>().index = index;
+                objects.Add(o);
             }
-
-            var objectSize = screenWidth / (float)(objects.Count + 2);
-
-            if (objectSize > screenHeight * 0.7f){
-                objectSize = screenHeight * 0.7f;
+            if (unit is EmptyUnit) {
+                var o = Instantiate(emptyPrefabs[((EmptyUnit)unit).prefabId]);
+                o.GetComponent<DungeonUnit>().Self = unit;
+                o.GetComponent<DungeonUnit>().gm = this;
+                o.GetComponent<DungeonUnit>().index = index;
+                objects.Add(o);
             }
+            ++index;
+        }
 
-            var objectLeft = new Vector3(lowerLeft.x  + objectSize * 0.5f, 0, 0);
-            var objectRight = new Vector3(lowerRight.x - objectSize * 0.5f, 0, 0);
+        var objectSize = screenWidth / (float)(objects.Count + 2);
 
-            for (int i = 0; i < objects.Count; ++i) {
-                objects[i].transform.localPosition = Vector3.Lerp(objectLeft, objectRight, (i)/((float)objects.Count-1));
-                objects[i].transform.localScale = new Vector3( objectSize, objectSize, 1);
-            }
+        if (objectSize > screenHeight * 0.7f){
+            objectSize = screenHeight * 0.7f;
+        }
+
+        var objectLeft = new Vector3(lowerLeft.x  + objectSize * 0.5f, 0, 0);
+        var objectRight = new Vector3(lowerRight.x - objectSize * 0.5f, 0, 0);
+
+        for (int i = 0; i < objects.Count; ++i) {
+            objects[i].transform.localPosition = Vector3.Lerp(objectLeft, objectRight, (i)/((float)objects.Count-1));
+            objects[i].transform.localScale = new Vector3( objectSize, objectSize, 1);
+        }
+        remainingAP = dungeon.ap;
+        waitForTurn = !dungeon.myTurn;
     }
 
     public void makeTargettableForPattern(Skill skill) {
@@ -164,7 +185,14 @@ public class DungeonController : MonoBehaviour
     }
 
     public void endTurn() {
-        remainingAP += 10;
+        if (dungeonId.isSome) {
+            DungeonAPI.endTurn(dungeonId.value, currentTurn);
+            currentTurn = new Turn{
+                skillsUsed = new List<SkillUsage>(),
+                turnId = 0
+            };
+            waitForTurn = true;
+        }
     }
 
     public void SkillWasUsed(int unitIndex, Skill skill) {
@@ -178,6 +206,8 @@ public class DungeonController : MonoBehaviour
 
 public class Dungeon {
     public List<Unit> units;
+    public bool myTurn;
+    public int ap;
 }
 
 public interface Unit {
@@ -198,11 +228,13 @@ public class EmptyUnit : Unit {
     public int prefabId;
 }
 
+[Serializable]
 public class Turn {
     public int turnId;
     public List<SkillUsage> skillsUsed;
 }
 
+[Serializable]
 public class SkillUsage {
     public int targetId;
     public Skill skill;
