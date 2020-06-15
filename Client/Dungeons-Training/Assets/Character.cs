31e651a2dd74694155513ad24ff08686d7a456a2
Julian Bieber <julian.bieber.1992@gmail.com>
2020-06-15 15:51:20 +0200

    select skills in character view

---
+++ Client/Dungeons-Training/Assets/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public Option<PlayerCharacter> player = Option<PlayerCharacter>.None;
+
+    private int nextUpdate = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Option<PlayerCharacter>.None;
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {   
+        if (Time.time >= nextUpdate) {
+            nextUpdate = (int)(Time.time) + 1;
+
+            var p = await CharacterAPI.getCharacter();
+            player = p;
+        }
+        
+    }
+}
---
+++ Client/Dungeons-Training/Assets/SkillDropDown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.UI.Dropdown;
+
+public class SkillDropDown : MonoBehaviour
+{
+
+    public Character character;
+
+    private Dropdown dropdown;
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character.player.isSome) {
+            dropdown.ClearOptions();
+            List<OptionData> options = new List<OptionData>(); 
+            options.Add(new OptionData());
+            foreach (var skill in character.player.value.skillBar.unlocked) {
+                options.Add(new OptionData(skill.id + "|" + skill.name));
+            }
+            dropdown.AddOptions(options);
+        } 
+    }
+
+    public void selectSkill(int i) {
+        var option = character.player.value.skillBar.unlocked[i - 1];
+        CharacterAPI.selectSkill(option.id);
+    }
+}
