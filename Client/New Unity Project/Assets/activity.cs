87a2a119d12127cab3fe616ed3741114d6910c45
Julian Bieber <julian.bieber.1992@gmail.com>
2020-08-24 14:18:56 +0200

    tracking app

--- Client/New Unity Project/Assets/activity.cs
+++ Client/New Unity Project/Assets/activity.cs
@@ -2,60 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Text;
 
-public class activity : MonoBehaviour
+public class Activity : MonoBehaviour
 {
 
-    public Button pullUpsButton;
-    public Button pushUpsButton;
-    public Button stopButton;
-    public Text text;
+    public Button self;
+    public Button other;
     public Text countDownText;
+    public string activity;
+    private bool isRecording = false;
+
+    private float countdownStop = -1;
+    private float nextFrame = -1;
+    private StreamWriter output;
+    private bool vibrated = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        pullUpsButton.gameObject.SetActive(true);
-        pushUpsButton.gameObject.SetActive(true);
-        text.gameObject.SetActive(true);
-        stopButton.gameObject.SetActive(false);
+        self.GetComponent<Image>().color = Color.green;
+        output = new StreamWriter(Application.persistentDataPath + "/"+ activity+".csv", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time < countdownStop) {
+            countDownText.text = (countdownStop - Time.time).ToString(); 
+        } else {
+            if (Time.time >= nextFrame && nextFrame > -1) {
+                if (!vibrated) {
+                    Handheld.Vibrate();
+                    vibrated = true;
+                }
+                Debug.Log("write line " + activity);
+                nextFrame += 0.1f;
+                StringBuilder line = new StringBuilder();
+                line.Append(activity);
+                line.Append(",");
+
+                line.Append(Input.gyro.userAcceleration.x);
+                line.Append(",");
+                line.Append(Input.gyro.userAcceleration.y);
+                line.Append(",");
+                line.Append(Input.gyro.userAcceleration.z);
+                line.Append(",");
+
+                line.Append(Input.gyro.attitude.x);
+                line.Append(",");
+                line.Append(Input.gyro.attitude.y);
+                line.Append(",");
+                line.Append(Input.gyro.attitude.z);
+                line.Append(",");
+                line.Append(Input.gyro.attitude.w);
+                line.Append(",");
+                
+                line.Append(Input.acceleration.x);
+                line.Append(",");
+                line.Append(Input.acceleration.y);
+                line.Append(",");
+                line.Append(Input.acceleration.z);
+                line.Append(",");
+                line.Append(Time.time);
+
+                output.WriteLine(line.ToString());
+                output.Flush();
+            }
+        }
+    }
+
+    public void toggle()
+    {
+        if (!isRecording) {
+            countdownStop = Time.time + 3;
+            nextFrame = countdownStop + 1;
+            other.gameObject.SetActive(false);
+            isRecording = true;
+            self.GetComponent<Image>().color = Color.red;
+            vibrated = false;
+        } else {
+            countdownStop = -1;
+            nextFrame = countdownStop;
+            isRecording = false;
+            self.GetComponent<Image>().color = Color.green;
+            other.gameObject.SetActive(true);
+        }
         
     }
 
-    public void StartActivity(string activity)
-    {
-        ToggleStartView();
-    }
-    
-    public void StopActivity()
-    {
-        ToggleStartView();
-        countDown();
-    }
 
-    public void ToggleStartView()
-    {
-        bool active = text.gameObject.activeSelf;
-        print(active);
-        pullUpsButton.gameObject.SetActive(!active);
-        pushUpsButton.gameObject.SetActive(!active);
-        text.gameObject.SetActive(!active);
-        stopButton.gameObject.SetActive(active);
-    }
-
-    void countDown()
-    {
-        int countDown = 3;
-        while (countDown>0);
-        {
-            countDownText.text = countDown.ToString();
-            print(countDownText.text);
-        }
-
-    }
 }
